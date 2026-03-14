efac2f7e53994ca86627f3eb213f092eac058efb
Ram Shrestha <shrestha23ram@gmail.com>
2026-03-14 13:26:27 +0545

    Ready for deployment

--- src/components/AllProject.jsx
+++ src/components/AllProject.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+
+import React, { useEffect } from "react";
 import { FaArrowLeft } from "react-icons/fa";
 import styles from "./AllProject.module.css";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import AllProjectItem from "./AllProjectItem";
 import { Link } from "react-router-dom";
+import { fetchProjects } from "../store/projectSlice";
 
 const AllProject = () => {
+  const dispatch = useDispatch();
   const ProItems = useSelector((store) => store.project);
+
+  useEffect(() => {
+    dispatch(fetchProjects());
+  }, [dispatch]);
 
   return (
     <>
@@ -18,17 +25,38 @@ const AllProject = () => {
             </span>
             <span className={styles.name}>Ram Shrestha</span>
           </Link>
+
           <h1 className={styles.h1}>All Projects</h1>
+
           <div className={`row py-4 ${styles.thead}`}>
-            <div className={`col-lg-1 col-md-2 col-sm-2 col-2 ${styles.year}`}><span>Year</span></div>
-            <div className={`col-lg-3 col-md-6 col-sm-6 col-10 ${styles.title}`}><span>Project</span></div>
-            <div className={`col-lg-2 col-md-0 col-sm-0 col-0 ${styles.made}`}><span>Made at</span></div>
-            <div className={`col-lg-4 col-md-0 col-sm-0 col-0 ${styles.languages}`}><span>Built with</span></div>
-            <div className={`col-lg-2 col-md-4 col-sm-4 col-0 ${styles.link}`}><span>Link</span></div>
+            <div className={`col-lg-1 col-md-2 col-sm-2 col-2 ${styles.year}`}>
+              <span>Year</span>
+            </div>
+
+            <div className={`col-lg-3 col-md-6 col-sm-6 col-10 ${styles.title}`}>
+              <span>Project</span>
+            </div>
+
+            <div className={`col-lg-2 d-none d-lg-block ${styles.made}`}>
+              <span>Made at</span>
+            </div>
+
+            <div className={`col-lg-4 d-none d-lg-block ${styles.languages}`}>
+              <span>Built with</span>
+            </div>
+
+            <div className={`col-lg-2 col-md-4 col-sm-4 d-none d-md-block ${styles.link}`}>
+              <span>Link</span>
+            </div>
           </div>
-          {ProItems.map((item) => (
-            <AllProjectItem key={item.id} item={item} />
-          ))}
+
+          {ProItems.length === 0 ? (
+            <p>Loading projects...</p>
+          ) : (
+            ProItems.map((item) => (
+              <AllProjectItem key={item.id} item={item} />
+            ))
+          )}
         </section>
       </div>
     </>
@@ -36,4 +64,3 @@ const AllProject = () => {
 };
 
 export default AllProject;
-
--- src/components/AllProjectItem.jsx
+++ src/components/AllProjectItem.jsx
@@ -1,25 +1,42 @@
+
 import React from "react";
 import { FaArrowRight } from "react-icons/fa";
 import styles from "./AllProject.module.css";
+
 const AllProjectItem = ({ item }) => {
   return (
     <>
       <div className={`row py-4 ${styles.bdr}`}>
-        <div className={`col-lg-1 col-md-2 col-2 ${styles.years}`}>{item.year}</div>
-        <div className={`col-lg-3 col-md-6 col-10 ${styles.titles}`}>{item.title}</div>
-        <div className={`col-lg-2 col-md-0 col-0 ${styles.mades}`}>{item.made}</div>
-        <div className={`col-lg-4 col-md-0 col-0 ${styles.languagess}`}>
+        <div className={`col-lg-1 col-md-2 col-2 ${styles.years}`}>
+          {item.year}
+        </div>
+
+        <div className={`col-lg-3 col-md-6 col-10 ${styles.titles}`}>
+          {item.title}
+        </div>
+
+        <div className={`col-lg-2 d-none d-lg-block ${styles.mades}`}>
+          {item.made}
+        </div>
+
+        <div className={`col-lg-4 d-none d-lg-block ${styles.languagess}`}>
           <ul className={styles.language}>
-            {item.language.language_name.map((language) => (
+            {item.language_name?.map((language) => (
               <li key={language} className={styles.language_name}>
                 {language}
               </li>
             ))}
           </ul>
         </div>
+
         <div className={`col-lg-2 col-md-4 col-4 ${styles.links}`}>
-          <a href="#" className={styles.links}>
-            <span> {item.link}</span>
+          <a
+            href={item.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.links}
+          >
+            <span>{item.link}</span>
             <span className={styles.link_arrow}>
               <FaArrowRight />
             </span>
