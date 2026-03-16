3e54218930f272ef6eb4b1124b816edd75a862ce
Ram Shrestha <shrestha23ram@gmail.com>
2026-03-16 15:10:52 +0545

    update spinner image

--- src/components/SpinnerImage.jsx
+++ src/components/SpinnerImage.jsx
@@ -5,6 +5,7 @@ import styles from "./SpinnerImage.module.css";
 import { Link } from "react-router-dom";
 import rotateGif from "../images/rotate.gif";
 
+
 const SpinnerImage = () => {
   const offcanvasImage = [
     {
